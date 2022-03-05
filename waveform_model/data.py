bbdc304c549fb822349db218f086fa7257bf1988
HeidiCheng <heidi8844@gmail.com>
2022-03-04 16:56:08 -0800

    waveform model - version 1

---
+++ waveform_model/data.py
@@ -0,0 +1,59 @@
+import os
+import torch
+import numpy as np
+import torchaudio
+import torchaudio.transforms as T
+
+from torch.utils.data import Dataset
+from tqdm import trange
+
+class PianoToGuitar(Dataset):
+
+    def __init__(self, directory, set_type, device):
+        list_fname = os.path.join(directory, set_type + '_set.txt')
+        #list_fname = os.path.join(directory, 'valid_set.txt')
+        sample_fpaths = []
+
+        with open(list_fname, 'r') as f:
+            for l in f.readlines():
+                l = l.strip().split(',')
+                dir_name = l[0]
+                f_name = l[1] + '.wav' 
+                sample_fpaths.append(os.path.join(dir_name, f_name))
+
+        self.samples = []
+
+        print("============= Load " + set_type + " data ==============")
+        for i in trange(len(sample_fpaths)):
+
+            # Load audio files
+            # Get the file paths for the piano sample and corresponding guitar sample
+            piano_fpath = os.path.join(directory, os.path.join('piano1_chords', sample_fpaths[i]))
+            guitar_fpath = os.path.join(directory, os.path.join('guitar1_chords', sample_fpaths[i]))
+             
+            piano_wav = torchaudio.load(piano_fpath)
+            guitar_wav = torchaudio.load(guitar_fpath)
+
+            resampler = T.Resample(piano_wav[1], 22050, dtype=piano_wav[0].dtype)
+
+            piano_wav_downsampled = resampler(piano_wav[0])
+            guitar_wav_downsampled = resampler(guitar_wav[0])
+
+            mono_piano_wav = torch.mean(piano_wav_downsampled, dim=0).unsqueeze(0)
+            mono_guitar_wav = torch.mean(guitar_wav_downsampled, dim=0).unsqueeze(0)
+
+            self.samples.append((mono_piano_wav, mono_guitar_wav))
+
+        self.device = device
+
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+
+        piano_wav, guitar_wav = self.samples[idx]
+
+        return piano_wav.to(self.device), \
+               guitar_wav.to(self.device)
+
---
+++ waveform_model/model.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+
+class WavNet(torch.nn.Module):
+    # WavNet (architecture for waveform conversion piano to guitar)
+
+    def __init__(self):
+        super(WavNet, self).__init__()
+
+        # Encoding/Downsampling conv blocks (4)
+        self.e1 = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=1),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.e2 = nn.Sequential(
+            nn.Conv1d(32, 64, kernel_size=1),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.e3 = nn.Sequential(
+            nn.Conv1d(64, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            #nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.e4 = nn.Sequential(
+            nn.Conv1d(128, 256, kernel_size=1),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            #nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+
+        # Transformer blocks (5)
+        self.t1 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.t2 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, padding=2, dilation=2),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.t3 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, padding=4, dilation=4),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.t4 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, padding=8, dilation=8),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.t5 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, padding=16, dilation=16),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        #Decoding/Upsampling deconv blocks(4)
+        self.d1 = nn.Sequential(
+            #nn.Upsample(scale_factor=2, mode='linear', align_corners=True),
+            nn.Conv1d(256, 128, kernel_size=1),
+            #nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.d2 = nn.Sequential(
+            #nn.Upsample(scale_factor=2, mode='linear', align_corners=True),
+            nn.Conv1d(128, 64, kernel_size=1),
+            #nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.d3 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='linear', align_corners=True),
+            nn.Conv1d(64, 32, kernel_size=1),
+            #nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.d4 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='linear', align_corners=True),
+            nn.Conv1d(32, 1, kernel_size=1),
+            #nn.BatchNorm2d(1),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+
+    def forward(self, x):
+
+        # Convolutional encoder
+        x = self.e1(x)
+        x = self.e2(x)
+        x = self.e3(x)
+        x = self.e4(x)
+
+        # Convolutional transformer
+        x = self.t1(x)
+        x = self.t2(x)
+        x = self.t3(x)
+        x = self.t4(x)
+        x = self.t5(x)
+
+        # Convolutional decoder
+        x = self.d1(x)
+        x = self.d2(x)
+        x = self.d3(x)
+        x = self.d4(x)
+
+        return x
